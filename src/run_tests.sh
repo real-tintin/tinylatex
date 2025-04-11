e63a42348f9da8543fd032378f7b7f38de7a4f2c
Oscar Goldring <oscar@goldring.dev>
2025-04-11 20:27:23 +0200

    Fixes of bash script and lint in pipeline.

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -4,5 +4,5 @@ IMAGE_NAME=tinylatex_tests
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build ${SCRIPT_DIR} -t ${IMAGE_NAME}
+docker build "${SCRIPT_DIR}" -t ${IMAGE_NAME}
 docker run ${IMAGE_NAME} python -m pytest .
